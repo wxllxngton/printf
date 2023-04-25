ccfbc161e159db842432ab1b9d6684ad9e586064
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-04-25 19:37:46 +0300

    Create main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+#include <string.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+#endif
